bf0427558ce0ff0c99df6c8ac84c291bbdb9a51b
Ovi Turcu <ovi_tsb@yahoo.com>
2017-10-27 10:41:07 -0400

    Paperclip settings for heroku

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,6 +2,8 @@ class Pin < ApplicationRecord
   acts_as_votable
   belongs_to :user
 
-  has_attached_file :image, styles: {medium: "300x300>"}
+  has_attached_file :image, styles: {medium: "300x300>"},
+    :url => "/assets/:basename.:extension",
+    :path => ":rails_root/app/assets/images/:basename.:extension"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
