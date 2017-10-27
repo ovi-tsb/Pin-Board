c29aba11774529cfc3ada2ac4d138ce3c66be0e6
Ovi Turcu <ovi_tsb@yahoo.com>
2017-10-27 11:03:02 -0400

    Paperclip option on development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,6 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   # needed for devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  Paperclip.options[:command_path] = "/usr/local/bin/"
+  # paperclip configuration for heroku
 end
